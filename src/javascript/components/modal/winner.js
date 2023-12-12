1e3b37e19c208cb63ffff9de32435d8031b9f6be
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-12 12:08:50 -0300

    + Adds logic to showWinnerModal().

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,11 @@
+import createElement from '../../helpers/domHelper';
 import showModal from './modal';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
+    const nameElement = createElement({
+        tagName: 'div'
+    });
+    nameElement.add(fighter.name);
+    showModal({ title: 'Winner', bodyElement: nameElement });
 }
