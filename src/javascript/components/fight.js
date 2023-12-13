43d6c0dbd07e7809718bb2afb0057761df6c14b0
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-13 15:50:22 -0300

    + Adds logic to handle hit combos.

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -2,27 +2,39 @@
 /* eslint-disable no-console */
 /* eslint-disable no-use-before-define */
 import controls from '../../constants/controls';
+import { createReferenceObject } from '../helpers/fightHelper';
 
-const pressedControls = {};
-Object.keys(controls).forEach(key => {
-    pressedControls[key] = false;
-});
+const pressedControls = createReferenceObject(Object.keys(controls));
+const criticalCombinations = createReferenceObject([
+    ...controls.PlayerOneCriticalHitCombination,
+    ...controls.PlayerTwoCriticalHitCombination
+]);
 // console.log('pressedControls?', pressedControls);
 
 export async function fight(firstFighter, secondFighter) {
+    let { PlayerOneCriticalHitCombination, PlayerTwoCriticalHitCombination } = pressedControls;
     let playerOneHealth = firstFighter.health;
     let playerTwoHealth = secondFighter.health;
     return new Promise(resolve => {
         // resolve the promise with the winner when fight is over
         if (playerTwoHealth >= 1 && playerOneHealth >= 1) {
             document.addEventListener('keydown', event => {
+                const isCriticalCombo = Object.keys(criticalCombinations).find(key => key === event.code);
                 const control = Object.keys(controls).find(key => controls[key] === event.code);
-                pressedControls[control] = true;
+                pressedControls[control] = !isCriticalCombo && control;
+                criticalCombinations[isCriticalCombo] = !!isCriticalCombo;
                 const isPlayerOne = /One/.test(control);
                 const isDefender = /Block/.test(control);
-                // console.log('pressedControls?', pressedControls);
+                const playerOneCriticalHit = controls.PlayerOneCriticalHitCombination.every(
+                    combo => criticalCombinations[combo] === true
+                );
+                const playerTwoCriticalHit = controls.PlayerTwoCriticalHitCombination.every(
+                    combo => criticalCombinations[combo] === true
+                );
+                PlayerOneCriticalHitCombination = playerOneCriticalHit;
+                PlayerTwoCriticalHitCombination = playerTwoCriticalHit;
                 let damage = 0;
-                if (!isDefender) {
+                if (!isDefender && (control || PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination)) {
                     damage = getDamage.apply(
                         this,
                         isPlayerOne ? [firstFighter, secondFighter] : [secondFighter, firstFighter]
@@ -37,12 +49,14 @@ export async function fight(firstFighter, secondFighter) {
             });
             document.addEventListener('keyup', event => {
                 const control = Object.keys(controls).find(key => controls[key] === event.code);
+                const isCriticalCombo = Object.keys(criticalCombinations).find(key => key === event.code);
                 pressedControls[control] = false;
-                console.log('pressedControls?', pressedControls);
+                criticalCombinations[isCriticalCombo] = false;
             });
         } else {
             // Cuando la promesa se resuelva, devolverá el objeto del fighter ganador:
             // De cómo mostrar el ganador se encarga el elemento que haya llamado a esta función.
+
             resolve(playerOneHealth > 0 ? firstFighter : secondFighter);
         }
     });
@@ -50,27 +64,26 @@ export async function fight(firstFighter, secondFighter) {
 
 export function getHitPower(fighter) {
     // return hit power
+    const { PlayerOneCriticalHitCombination, PlayerTwoCriticalHitCombination } = pressedControls;
     const critialHitChance = Math.floor(Math.random() * 2) + 1;
     const hitPower = fighter.attack * critialHitChance;
     const criticalStrike = fighter.attack * 2;
-    // console.log('hitPower?', hitPower);
-    return hitPower;
+    return PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? criticalStrike : hitPower;
 }
 
 export function getBlockPower(fighter) {
     // return block power
+    const { PlayerOneCriticalHitCombination, PlayerTwoCriticalHitCombination } = pressedControls;
     const dodgeChance = Math.floor(Math.random() * 2) + 1;
     const power = fighter.defense * dodgeChance;
-    // console.log('power?', power);
-    return power;
+    return PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? 0 : power;
 }
 
 export function getDamage(attacker, defender) {
     // return damage
-    // console.log(getHitPower(attacker) - getBlockPower(defender));
+    // console.log('state of pressedControls', pressedControls);
     const { PlayerOneBlock, PlayerTwoBlock } = pressedControls;
     const isBlocking = PlayerOneBlock || PlayerTwoBlock;
-    // const damage = isBlocking ? getHitPower(attacker) - getBlockPower(defender) : getHitPower(attacker);
     const damage = getHitPower(attacker) - (isBlocking ? getBlockPower(defender) : 0);
     return damage > 0 ? damage : 0;
 }
