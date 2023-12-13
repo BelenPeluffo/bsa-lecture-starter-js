2c259cd870f795b890ef122411ddec89b42be335
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-13 11:39:08 -0300

    + Adds logic to determine whether player is attacking or blocking and calling getDamage() accordingly.

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-use-before-define */
 import controls from '../../constants/controls';
 
@@ -9,31 +10,31 @@ Object.keys(controls).forEach(key => {
 
 export async function fight(firstFighter, secondFighter) {
     const playerOneHealth = firstFighter.health;
-    let playerTwoHealth = secondFighter.health;
+    const playerTwoHealth = secondFighter.health;
     return new Promise(resolve => {
         // resolve the promise with the winner when fight is over
         if (playerTwoHealth >= 1 && playerOneHealth >= 1) {
             document.addEventListener('keydown', event => {
                 const control = Object.keys(controls).find(key => controls[key] === event.code);
                 pressedControls[control] = true;
-                // console.log('pressedControls?', pressedControls);
+                const isPlayerOne = /One/.test(control);
+                const isDefender = /Block/.test(control);
+                console.log('pressedControls?', pressedControls);
                 let damage = 0;
-                if (Object.values(controls).includes(event.code)) {
-                    const isPlayerOne = /One/.test(control);
-                    // const isDefender = /Block/.test(control);
-                    // console.log('isPlayerOne?', isPlayerOne);
+                if (!isDefender) {
                     damage = getDamage.apply(
                         this,
                         isPlayerOne ? [firstFighter, secondFighter] : [secondFighter, firstFighter]
                     );
-                    playerTwoHealth -= damage > 0 ? damage : 0;
+                    console.log('damage?', damage);
+                    // playerTwoHealth -= damage > 0 ? damage : 0;
                     damage = 0;
                 }
             });
             document.addEventListener('keyup', event => {
                 const control = Object.keys(controls).find(key => controls[key] === event.code);
                 pressedControls[control] = false;
-                // console.log('pressedControls?', pressedControls);
+                console.log('pressedControls?', pressedControls);
             });
         } else {
             // Cuando la promesa se resuelva, devolverá el objeto del fighter ganador:
@@ -47,6 +48,7 @@ export function getHitPower(fighter) {
     // return hit power
     const critialHitChance = Math.floor(Math.random() * 2) + 1;
     const hitPower = fighter.attack * critialHitChance;
+    // const criticalStrike = fighter.attack * 2;
     // console.log('hitPower?', hitPower);
     return hitPower;
 }
