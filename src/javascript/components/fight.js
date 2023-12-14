88fdf201102927ae7e21bb7e2daa20128d7ebb53
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-14 13:52:17 -0300

    + Refactors code and creates proper logic to determine when game finishes.

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -16,145 +16,88 @@ export async function fight(firstFighter, secondFighter) {
     let playerOneHealth = firstFighter.health;
     let playerTwoHealth = secondFighter.health;
 
-    const keyDownListener = event => {
-        // console.log('event.code?', event.code);
-        // console.log('pressedControls?', pressedControls);
-        const isRegularControl = Object.keys(controls).find(key => controls[key] === event.code);
-        const isComboControl = Object.keys(criticalCombinations).includes(event.code);
-        console.log('isComboControl?', isComboControl);
-
-        if (isRegularControl) setControlState(controls, true, pressedControls, 'key', event.code);
-        else if (!isRegularControl) {
-            setControlState(criticalCombinations, true, null, 'value', event.code);
-            console.log('isComboControl!');
-            PlayerOneCriticalHitCombination = setControlState(
-                controls.PlayerOneCriticalHitCombination,
-                true,
-                criticalCombinations
-            );
-            PlayerTwoCriticalHitCombination = setControlState(
-                controls.PlayerTwoCriticalHitCombination,
-                true,
-                criticalCombinations
-            );
-        }
-
-        const isPlayerOne = /One/.test(isRegularControl) || PlayerOneCriticalHitCombination;
-        const isDefender = /Block/.test(isRegularControl);
-        // console.log('criticalCombinations?', criticalCombinations);
-        let damage = 0;
-        if (!isDefender && (isRegularControl || PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination)) {
-            damage = getDamage.apply(this, isPlayerOne ? [firstFighter, secondFighter] : [secondFighter, firstFighter]);
-            console.log('isPlayerOne?', isPlayerOne);
-            // console.log('PlayerOneCriticalHitCombination?', PlayerOneCriticalHitCombination);
-            // console.log('PlayerTwoCriticalHitCombination?', PlayerTwoCriticalHitCombination);
-            console.log('damage?', damage);
-            if (isPlayerOne) {
-                playerTwoHealth -= isPlayerOne ? damage : 0;
-                updateHealthBar('right', secondFighter.health, playerTwoHealth);
-            } else {
-                playerOneHealth -= !isPlayerOne || PlayerTwoCriticalHitCombination ? damage : 0;
-                updateHealthBar('left', firstFighter.health, playerOneHealth);
-            }
-            damage = 0;
-            console.log('playerTwoHealth?', playerTwoHealth);
-            console.log('playerOneHealth?', playerOneHealth);
-        }
-    };
-
-    const keyUpListener = event => {
-        console.log('============KEY-UP==============');
-        setControlState(controls, false, pressedControls, 'key', event.code);
-        setControlState(criticalCombinations, false, null, 'value', event.code);
-        // PlayerOneCriticalHitCombination = setControlState(
-        //     controls.PlayerOneCriticalHitCombination,
-        //     false,
-        //     criticalCombinations
-        // );
-        // PlayerTwoCriticalHitCombination = setControlState(
-        //     controls.PlayerTwoCriticalHitCombination,
-        //     false,
-        //     criticalCombinations
-        // );
-    };
-
     return new Promise(resolve => {
         // resolve the promise with the winner when fight is over
-        resolve(playerOneHealth > 0 ? firstFighter : secondFighter);
-        if (playerTwoHealth < +1 && playerOneHealth < +1) {
-            // Cuando la promesa se resuelva, devolverá el objeto del fighter ganador:
-            // De cómo mostrar el ganador se encarga el elemento que haya llamado a esta función.
 
-            resolve(playerOneHealth > 0 ? firstFighter : secondFighter);
-        } else {
-            document.addEventListener('keydown', keyDownListener);
-            document.addEventListener('keydown', keyUpListener);
-            //     // console.log('event.code?', event.code);
-            //     // console.log('pressedControls?', pressedControls);
-            //     const isRegularControl = Object.keys(controls).find(key => controls[key] === event.code);
-            //     const isComboControl = Object.keys(criticalCombinations).includes(event.code);
-            //     console.log('isComboControl?', isComboControl);
+        const keyDownListener = event => {
+            console.log('keyDownListener');
+            // console.log('event.code?', event.code);
+            // console.log('pressedControls?', pressedControls);
+            const isRegularControl = Object.keys(controls).find(key => controls[key] === event.code);
+            const isComboControl = Object.keys(criticalCombinations).includes(event.code);
+            // console.log('isComboControl?', isComboControl);
 
-            //     if (isRegularControl) setControlState(controls, true, pressedControls, 'key', event.code);
-            //     else if (!isRegularControl) {
-            //         setControlState(criticalCombinations, true, null, 'value', event.code);
-            //         console.log('isComboControl!');
-            //         PlayerOneCriticalHitCombination = setControlState(
-            //             controls.PlayerOneCriticalHitCombination,
-            //             true,
-            //             criticalCombinations
-            //         );
-            //         PlayerTwoCriticalHitCombination = setControlState(
-            //             controls.PlayerTwoCriticalHitCombination,
-            //             true,
-            //             criticalCombinations
-            //         );
-            //     }
+            if (isRegularControl) setControlState(controls, true, pressedControls, 'key', event.code);
+            else if (!isRegularControl) {
+                setControlState(criticalCombinations, true, null, 'value', event.code);
+                // console.log('isComboControl!');
+                PlayerOneCriticalHitCombination = setControlState(
+                    controls.PlayerOneCriticalHitCombination,
+                    true,
+                    criticalCombinations
+                );
+                PlayerTwoCriticalHitCombination = setControlState(
+                    controls.PlayerTwoCriticalHitCombination,
+                    true,
+                    criticalCombinations
+                );
+            }
 
-            //     const isPlayerOne = /One/.test(isRegularControl) || PlayerOneCriticalHitCombination;
-            //     const isDefender = /Block/.test(isRegularControl);
-            //     // console.log('criticalCombinations?', criticalCombinations);
-            //     let damage = 0;
-            //     if (
-            //         !isDefender &&
-            //         (isRegularControl || PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination)
-            //     ) {
-            //         damage = getDamage.apply(
-            //             this,
-            //             isPlayerOne ? [firstFighter, secondFighter] : [secondFighter, firstFighter]
-            //         );
-            //         console.log('isPlayerOne?', isPlayerOne);
-            //         // console.log('PlayerOneCriticalHitCombination?', PlayerOneCriticalHitCombination);
-            //         // console.log('PlayerTwoCriticalHitCombination?', PlayerTwoCriticalHitCombination);
-            //         console.log('damage?', damage);
-            //         if (isPlayerOne) {
-            //             playerTwoHealth -= isPlayerOne ? damage : 0;
-            //             updateHealthBar('right', secondFighter.health, playerTwoHealth);
-            //         } else {
-            //             playerOneHealth -= !isPlayerOne || PlayerTwoCriticalHitCombination ? damage : 0;
-            //             updateHealthBar('left', firstFighter.health, playerOneHealth);
-            //         }
-            //         damage = 0;
-            //         console.log('playerTwoHealth?', playerTwoHealth);
-            //         console.log('playerOneHealth?', playerOneHealth);
-            //     }
-            // });
-            // document.addEventListener('keyup', event => {
-            //     console.log('============KEY-UP==============');
-            //     setControlState(controls, false, pressedControls, 'key', event.code);
-            //     setControlState(criticalCombinations, false, null, 'value', event.code);
-            //     // PlayerOneCriticalHitCombination = setControlState(
-            //     //     controls.PlayerOneCriticalHitCombination,
-            //     //     false,
-            //     //     criticalCombinations
-            //     // );
-            //     // PlayerTwoCriticalHitCombination = setControlState(
-            //     //     controls.PlayerTwoCriticalHitCombination,
-            //     //     false,
-            //     //     criticalCombinations
-            //     // );
-            // });
-        }
+            const isPlayerOne = /One/.test(isRegularControl) || PlayerOneCriticalHitCombination;
+            const isDefender = /Block/.test(isRegularControl);
+            // console.log('criticalCombinations?', criticalCombinations);
+            let damage = 0;
+            if (
+                !isDefender &&
+                (isRegularControl || PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination)
+            ) {
+                damage = getDamage.apply(
+                    this,
+                    isPlayerOne ? [firstFighter, secondFighter] : [secondFighter, firstFighter]
+                );
+                // console.log('isPlayerOne?', isPlayerOne);
+                // console.log('PlayerOneCriticalHitCombination?', PlayerOneCriticalHitCombination);
+                // console.log('PlayerTwoCriticalHitCombination?', PlayerTwoCriticalHitCombination);
+                console.log('damage?', damage);
+                if (isPlayerOne) {
+                    playerTwoHealth -= isPlayerOne ? damage : 0;
+                    updateHealthBar('right', secondFighter.health, playerTwoHealth);
+                } else {
+                    playerOneHealth -= !isPlayerOne || PlayerTwoCriticalHitCombination ? damage : 0;
+                    updateHealthBar('left', firstFighter.health, playerOneHealth);
+                }
+                damage = 0;
+                if (playerTwoHealth < 1 || playerOneHealth < 1) {
+                    // Resolve the promise with the winner when the fight is over
+                    console.log('Game over');
+                    document.removeEventListener('keydown', keyDownListener);
+                    document.removeEventListener('keyup', keyUpListener);
+                    resolve(playerOneHealth > 0 ? firstFighter : secondFighter);
+                }
+                console.log('playerTwoHealth?', playerTwoHealth);
+                console.log('playerOneHealth?', playerOneHealth);
+            }
+        };
+
+        const keyUpListener = event => {
+            console.log('============KEY-UP==============');
+            setControlState(controls, false, pressedControls, 'key', event.code);
+            setControlState(criticalCombinations, false, null, 'value', event.code);
+            // PlayerOneCriticalHitCombination = setControlState(
+            //     controls.PlayerOneCriticalHitCombination,
+            //     false,
+            //     criticalCombinations
+            // );
+            // PlayerTwoCriticalHitCombination = setControlState(
+            //     controls.PlayerTwoCriticalHitCombination,
+            //     false,
+            //     criticalCombinations
+            // );
+        };
+
+        console.log('resolve?', resolve);
+        document.addEventListener('keydown', keyDownListener);
+        document.addEventListener('keyup', keyUpListener);
     });
 }
 
@@ -164,30 +107,30 @@ export function getHitPower(fighter) {
     const critialHitChance = Math.floor(Math.random() * 2) + 1;
     const hitPower = fighter.attack * critialHitChance;
     const criticalStrike = fighter.attack * 2;
-    console.log(
-        'hitPower?',
-        PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? criticalStrike : hitPower
-    );
+    // console.log(
+    //     'hitPower?',
+    //     PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? criticalStrike : hitPower
+    // );
     return PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? criticalStrike : hitPower;
 }
 
 export function getBlockPower(fighter) {
     // return block power
     const { PlayerOneCriticalHitCombination, PlayerTwoCriticalHitCombination } = pressedControls;
-    console.log('PlayerOneCriticalHitCombination?', PlayerOneCriticalHitCombination);
-    console.log('PlayerTwoCriticalHitCombination?', PlayerTwoCriticalHitCombination);
+    // console.log('PlayerOneCriticalHitCombination?', PlayerOneCriticalHitCombination);
+    // console.log('PlayerTwoCriticalHitCombination?', PlayerTwoCriticalHitCombination);
     const dodgeChance = Math.floor(Math.random() * 2) + 1;
     const power = fighter.defense * dodgeChance;
-    console.log('blockPower?', PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? 0 : power);
+    // console.log('blockPower?', PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? 0 : power);
     return PlayerOneCriticalHitCombination || PlayerTwoCriticalHitCombination ? 0 : power;
 }
 
 export function getDamage(attacker, defender) {
     // return damage
-    console.log('state of pressedControls', pressedControls);
+    // console.log('state of pressedControls', pressedControls);
     const { PlayerOneBlock, PlayerTwoBlock } = pressedControls;
     const isBlocking = PlayerOneBlock || PlayerTwoBlock;
-    console.log('isBlocking?', isBlocking);
+    // console.log('isBlocking?', isBlocking);
     const damage = getHitPower(attacker) - (isBlocking ? getBlockPower(defender) : 0);
     return damage > 0 ? damage : 0;
 }
